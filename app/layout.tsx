a7937e0036d27235e70c0fd355d0866b1f56d62f
Alvin Yeboah <49616291+alvinyeboah@users.noreply.github.com>
2025-07-09 15:36:37 +0000

    Update layout.tsx
    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <script defer src="https://analytics.alvinyeboah.com/script.js" data-website-id="e4465062-4ab2-4957-bddf-e55574614239"></script>
       <body className={`${inter.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
           {children}
